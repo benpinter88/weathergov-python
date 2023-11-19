d764a44886a7804598269418da0407150190de72
benpinter88 <50327569+benpinter88@users.noreply.github.com>
2023-11-19 16:00:10 -0500

    Update flask_webapp.py
    adding some comments

--- src/flask_webapp.py
+++ src/flask_webapp.py
@@ -4,6 +4,7 @@ import requests
 
 app = Flask(__name__)
 
+# Get lat, lon with geopy Nominatim api client
 def get_location_coordinates(location_name):
     geolocator = Nominatim(user_agent="weather_app")
     location = geolocator.geocode(location_name)
@@ -11,13 +12,14 @@ def get_location_coordinates(location_name):
         return location.latitude, location.longitude
     else:
         return None, None
-
+# Get the forecast URL based on lat, lon
 def get_forecast_url(lat, lon):
     url = f"https://api.weather.gov/points/{lat},{lon}"
     response = requests.get(url)
     data = response.json()
     return data['properties']['forecast']
 
+# Get the forecast data from grid URL we received in get_forecast_ur
 def get_forecast(forecast_url):
     response = requests.get(forecast_url)
     data = response.json()
@@ -25,7 +27,7 @@ def get_forecast(forecast_url):
 
 @app.route('/')
 def index():
-    location_name = "Boston, MA"  # or use a form to get user input
+    location_name = "Boston, MA"  # replace with your desired location
     lat, lon = get_location_coordinates(location_name)
     if lat and lon:
         forecast_url = get_forecast_url(lat, lon)
